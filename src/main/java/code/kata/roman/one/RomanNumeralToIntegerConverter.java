c3e7b92f73dc7cbc2f9ca2a11e9ec9aeb3f8e11b
Simone Bächle <yevonna@gmail.com>
2020-06-30 06:32:18 +0000

    Stand 29.06.2020

--- src/main/java/code/kata/roman/one/RomanNumeralToIntegerConverter.java
+++ src/main/java/code/kata/roman/one/RomanNumeralToIntegerConverter.java
@@ -3,6 +3,25 @@ package code.kata.roman.one;
 public class RomanNumeralToIntegerConverter {
 
 	public Integer convert(String romanNumeral) {
-		return null;
-	}
+    
+		if (romanNumeral == null) {
+      return null;
+    }     
+    
+    switch (romanNumeral) {
+      case "V": return 5;
+      case "I": return 1;
+      case "X": return 10;
+      case "L": return 50;
+      case "C": return 100;
+      case "D": return 500;
+      case "M": return 1000;
+      case "II": return 2;
+      default:  return null;
+    }
+  }
 }
+// Klaus: Läuft :)
+
+// kommt hier die main-methode?
+// Nope. Funktioniert hier ein bisschen anders. Wir konzentrieren uns auf eine Klasse plus Test und lassen Maven und das Surefire-Plugin im Hintergrund alles weitere machen
--- src/test/java/code/kata/roman/one/RomanNumeralToIntegerConverterTest.java
+++ src/test/java/code/kata/roman/one/RomanNumeralToIntegerConverterTest.java
@@ -15,26 +15,60 @@ public class RomanNumeralToIntegerConverterTest {
 		assertThat(result).isNull();
 	}
 
-	@Ignore("To be implemented")
 	@Test
 	public void test_convert_GivenRomanI_ThenReturn1() {
 		Integer result = converter.convert("I");
-		assertThat(result).isNull();
+		assertThat(result).isEqualTo(1);
 	}
 
 	// Test V -> 5
+  @Test
+	public void test_convert_GivenRomanV_ThenReturn5() {
+		Integer result = converter.convert("V");
+		assertThat(result).isEqualTo(5);
+	}
 
 	// Test X -> 10
+  @Test
+	public void test_convert_GivenRomanX_ThenReturn10() {
+		Integer result = converter.convert("X");
+		assertThat(result).isEqualTo(10);
+	}
 
 	// Test L -> 50
+  @Test
+	public void test_convert_GivenRomanL_ThenReturn50() {
+		Integer result = converter.convert("L");
+		assertThat(result).isEqualTo(50);
+	}
 
 	// Test C -> 100
+    @Test
+	public void test_convert_GivenRomanC_ThenReturn100() {
+		Integer result = converter.convert("C");
+		assertThat(result).isEqualTo(100);
+	}
 
 	// Test D -> 500
+  @Test
+	public void test_convert_GivenRomanD_ThenReturn500() {
+		Integer result = converter.convert("D");
+		assertThat(result).isEqualTo(500);
+	}
 
 	// Test M -> 1000
-
+  @Test
+	public void test_convert_GivenRomanM_ThenReturn1000() {
+		Integer result = converter.convert("M");
+		assertThat(result).isEqualTo(1000);
+	}
+  
 	// Test II -> 2
+  @Test
+	public void test_convert_GivenRomanII_ThenReturn2() {
+		Integer result = converter.convert("II");
+		assertThat(result).isEqualTo(2);
+	}
 
 	// Test III -> 3
 
@@ -58,5 +92,5 @@ public class RomanNumeralToIntegerConverterTest {
 
 	// Test CM -> 900
 
-	// Fehler bei allem außer I, V, X, L, C, D, M
+	// Fehler bei allem auï¿½er I, V, X, L, C, D, M
 }

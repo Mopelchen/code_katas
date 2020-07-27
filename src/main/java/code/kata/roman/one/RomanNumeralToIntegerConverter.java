d4e170f61751083d4b8b71b8f128d8f29e6ff89b
Mopelchen <yevonna@googlemail.com>
2020-07-27 18:03:12 +0200

    Make code a little bit nicer

--- src/main/java/code/kata/roman/one/RomanNumeralToIntegerConverter.java
+++ src/main/java/code/kata/roman/one/RomanNumeralToIntegerConverter.java
@@ -1,41 +1,36 @@
 package code.kata.roman.one;
 
 public class RomanNumeralToIntegerConverter {
-
+    
     public Integer convert(String romanNumeral) {
 
-        if (romanNumeral == null || romanNumeral.isEmpty()) {
-            return null;
+      if (romanNumeral == null || romanNumeral.isEmpty()) {
+          return null;
+      }
+
+      Integer summe = 0;
+
+      System.out.println("-----------------------------------------");
+      System.out.println(romanNumeral);
+      for (int i = 0; i < romanNumeral.length(); i++){
+
+        String current = romanNumeral.substring(i, i+1);
+        boolean nextSignExists = i < romanNumeral.length() - 1;
+
+        System.out.println("Index:" +i);
+        System.out.println("Aktuelles Zeichen: " + current);
+
+        if(nextSignExists && 
+            isNextLargerThanCurrent(current, getNext(romanNumeral, i))) {
+            summe -= convertToInt(current);
+            System.out.println(summe);
+        } else {
+            summe += convertToInt(current);
+            System.out.println(summe);
         }
+      }
 
-        Integer summe = 0;
-
-        // IV
-        System.out.println("-----------------------------------------");
-        System.out.println(romanNumeral);
-        for (int i = 0; i < romanNumeral.length(); i++){
-            if(i < romanNumeral.length() - 1){
-
-                System.out.println("Index:" +i);
-                System.out.println("Aktuelles Zeichen: " +romanNumeral.substring(i, i+1));
-
-                if(convertToInt(romanNumeral.substring(i+1, i+2)) > convertToInt(romanNumeral.substring(i, i+1))){
-                    summe -= convertToInt(romanNumeral.substring(i, i+1));
-                    System.out.println(summe);
-                }
-                else{
-                    summe += convertToInt(romanNumeral.substring(i, i+1));
-                    System.out.println(summe);
-                }
-            }
-            else{
-                summe += convertToInt(romanNumeral.substring(i, i+1));
-                System.out.println(summe);
-            }
-
-        }
-
-        return summe;
+      return summe;
     }
 
     private Integer convertToInt(String romanNumeral) {
@@ -57,5 +52,13 @@ public class RomanNumeralToIntegerConverter {
             default:
                 return null;
         }
+    }
+
+    private boolean isNextLargerThanCurrent(String current, String next) {
+      return convertToInt(next) > convertToInt(current);
+    }
+
+    private String getNext(String romanNumeral, int index) {
+      return romanNumeral.substring(index+1, index+2);
     }
 }
